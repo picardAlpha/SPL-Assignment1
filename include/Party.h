45ca0b7e9be4d4c861063bebc5fd1f5009380830
Lidor Test <lidor92@gmail.com>
2022-11-19 12:42:17 +0200

    clean copy with added offers array in party

---
+++ include/Party.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <string>
+#include <vector>
+
+using std::string;
+
+class JoinPolicy;
+class Simulation;
+
+enum State
+{
+    Waiting,
+    CollectingOffers,
+    Joined
+};
+
+class Party
+{
+public:
+    Party(int id, string name, int mandates, JoinPolicy *); 
+
+    State getState() const;
+    void setState(State state);
+    int getMandates() const;
+    void step(Simulation &s);
+    const string &getName() const;
+
+private:
+    int mId;
+    string mName;
+    int mMandates;
+    JoinPolicy *mJoinPolicy;
+    State mState;
+
+    //added
+
+    // Saves the Agents IDs that made an offer to this party by chronological order.
+    std::vector<int> offers;
+};
